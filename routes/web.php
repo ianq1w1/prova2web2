33aa6e675cdfda2b62f931674f12453baab8fe2a
ianq1w1 <ianpereiraian2@gmail.com>
2023-07-07 08:26:44 -0300

    primeiro erro: ausencia do import do SongController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PlaylistController;
+use App\Http\Controllers\SongController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
